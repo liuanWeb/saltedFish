e29bfbe3de889c49cf5779293db36330a919921d
tuiqiu <438247026@qq.com>
2019-06-24 21:17:13 +0800

    last

--- README.md
+++ README.md
@@ -21,5 +21,9 @@ npm run build
 - 拷贝项目：git clone 'github地址' 
 - 添加文件：git add .
 - 提交信息：git commit -m '注释'
-- 上传更新：ggit push -u origin master
+- 上传更新：git push -u origin master
 ```
+
+## 共享文件地址
+- [API接口地址]()
+- [基础菜单表结构](https://docs.qq.com/sheet/DTW5mdExUc01MQUhO?tdsourcetag=s_macqq_aiomsg&tab=BB08J2&coord=E3%24E3%240%240%241%240)
--- src/components/pages/homePage.vue
+++ src/components/pages/homePage.vue
@@ -31,37 +31,14 @@
         </div>
         <div class="content">
             <Menu active-name="1-1" :open-names="['1']">
-                <Submenu name="1">
+                <Submenu :name="item.MenuId" v-for="(item,index) in menuData" :key="index">
                     <template slot="title">
                         <Icon type="ios-paper" />
-                        内容管理
+                        {{item.MenuName}}
                     </template>
                     <MenuItem name="1-1">文章管理</MenuItem>
                     <MenuItem name="1-2">评论管理</MenuItem>
                     <MenuItem name="1-3">举报管理</MenuItem>
-                </Submenu>
-                <Submenu name="2">
-                    <template slot="title">
-                        <Icon type="ios-people" />
-                        用户管理
-                    </template>
-                    <MenuItem name="2-1">新增用户</MenuItem>
-                    <MenuItem name="2-2">活跃用户</MenuItem>
-                </Submenu>
-                <Submenu name="3">
-                    <template slot="title">
-                        <Icon type="ios-stats" />
-                        统计分析
-                    </template>
-                    <MenuGroup title="使用">
-                        <MenuItem name="3-1">新增和启动</MenuItem>
-                        <MenuItem name="3-2">活跃分析</MenuItem>
-                        <MenuItem name="3-3">时段分析</MenuItem>
-                    </MenuGroup>
-                    <MenuGroup title="留存">
-                        <MenuItem name="3-4">用户留存</MenuItem>
-                        <MenuItem name="3-5">流失用户</MenuItem>
-                    </MenuGroup>
                 </Submenu>
             </Menu>
             <router-view class="page" />
@@ -74,9 +51,7 @@
 export default {
     name: 'homePage',
     created(){
-        this.$api.homePageApi.getMenu().then(data=>{
-            this.menuData = data.data;
-        });
+        this.getMenu();
     },
     data () {
         return {
@@ -84,6 +59,11 @@ export default {
         }
     },
     methods: {
+        getMenu(){
+            this.$api.homePageApi.getMenu().then(data=>{
+                this.menuData = data.data;
+            });
+        },
         signOut(){
             this.$router.push('/loginPage');
             this.$Message.warning('已退出登录！')
